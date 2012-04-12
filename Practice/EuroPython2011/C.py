c2067a7d9d680b58ea2a3b7b7cba2f699bd3f718
Wesley Petrowski <wesley@petrowski.ca>
2012-04-12 13:39:07 -0500

    tentative solution (no formatted output)

--- Practice/EuroPython2011/C.py
+++ Practice/EuroPython2011/C.py
@@ -2,9 +2,27 @@
 
 import sys
 
-N = int(sys.stdin.readline())
+def checkIfSpell(word, start_ix, end_ix, orig_end_ix, curSyl, repeatedSyllables, numSyls):
+    if start_ix + len(curSyl) >= orig_end_ix:
+        return False
 
-vowels = 'aeiou'
+    inner = word[start_ix+len(curSyl):orig_end_ix]
+    if len(inner) == 0:
+        return False
+
+    if len(filter(lambda x: x in "aeiou", inner)) > 0 and numSyls > 1:
+        return True
+
+    new_start = start_ix + len(curSyl)
+    new_end = end_ix + len(curSyl)
+
+    #find another repeated syllable that has instances at start + len and end + len
+    for newSyl in filter(lambda x: new_start in x[1] and new_end in x[1], repeatedSyllables):
+        if checkIfSpell(word, new_start, new_end, orig_end_ix, newSyl[0], repeatedSyllables, numSyls+1):
+            return True
+    return False
+
+N = int(sys.stdin.readline())
 
 for i in xrange(N):
     expr = sys.stdin.readline().strip()
@@ -26,7 +44,24 @@ for i in xrange(N):
 
     repeatedSyllables = filter(lambda x: len(x[1]) > 1, d.items())
 
+    found = False
+    for syl in repeatedSyllables:
+        if found:
+            break
+        for i in xrange(len(syl[1]) - 1):
+            if found:
+                break
+            for j in xrange(i+1, len(syl[1])):
+                if found:
+                    break
+                start_ix = syl[1][i]
+                end_ix = syl[1][j]
+                numSyls = 1
+
+                found = checkIfSpell(expr, start_ix, end_ix, end_ix, syl[0], repeatedSyllables, numSyls)
+
     print 'Case #%d:' % (i+1)
     print expr
-    print sorted(d.items(), key=lambda x: x[1])
-    print sorted(repeatedSyllables, key=lambda x: x[1])
+    print found
+    #print sorted(d.items(), key=lambda x: x[1])
+    #print sorted(repeatedSyllables, key=lambda x: x[1])
